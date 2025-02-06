b7c9b3f57093badaea724f77a47dd1d8e6050f5a
Sir Blob <76974209+GamerBoss101@users.noreply.github.com>
2025-02-06 10:33:16 -0500

    Child Render Fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
     import { injectAnalytics } from '@vercel/analytics/sveltekit';
     import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+    let { children } = $props();
 
     injectSpeedInsights();
     injectAnalytics();
 </script>
+
+{@render children()}
