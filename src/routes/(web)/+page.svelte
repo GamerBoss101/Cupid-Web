7f9c1e72c1c5c8271742e81361dfb5f127096a24
Sir Blob <76974209+GamerBoss101@users.noreply.github.com>
2025-02-07 02:00:43 -0500

    Font Family Fix

--- src/routes/(web)/+page.svelte
+++ src/routes/(web)/+page.svelte
@@ -10,7 +10,7 @@
 </script>
 
 
-<main class="container mx-auto space-y-10 px-4 py-10">
+<main class="container mx-auto space-y-10 px-4 py-20">
     <img class="w-1/6 mx-auto" src="/cupid.png" alt="cupid"/>
     <h1 class="text-4xl font-semibold text-center">{currentMessage}</h1>
 </main>
